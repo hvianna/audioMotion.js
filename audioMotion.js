22325ab3c6c7a68ff2fec79f3d0dabe7015230fe
hvianna <hvianna@gmail.com>
2018-12-01 12:55:52 -0200

    version updated for development.

--- audioMotion.js
+++ audioMotion.js
@@ -19,7 +19,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-var _VERSION = '18.11';
+
+var _VERSION = '18.12-dev';
 
 
 /**
