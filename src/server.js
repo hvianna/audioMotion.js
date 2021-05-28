7092a85bd762cf4f322280c99ac97fe8771a696d
Henrique Vianna <hvianna@gmail.com>
2021-05-28 14:45:43 -0300

    Add server options to set or disable the backgrounds directory.

--- src/server.js
+++ src/server.js
@@ -19,10 +19,18 @@ const fs           = require('fs'),
 const imageExtensions = /\.(jpg|jpeg|webp|avif|png|gif|bmp)$/i;
 const audioExtensions = /\.(mp3|flac|m4a|aac|ogg|wav|m3u|m3u8)$/i;
 
-var port = 8000;
-var host = 'localhost';
-var launchClient = true;
-var musicPath = '';
+const BG_DIR = '/backgrounds'; // path to backgrounds folder (should start with a slash)
+
+// ANSI escape sequences for console colors - thanks https://stackoverflow.com/a/41407246
+const ANSI_RED   = '\x1b[31m',
+	  ANSI_GREEN = '\x1b[32m',
+	  ANSI_RESET = '\x1b[0m';
+
+let port            = 8000,
+	host            = 'localhost',
+	launchClient    = true,
+	musicPath       = '',
+	backgroundsPath = '';
 
 function showHelp() {
 	console.log( `
@@ -30,10 +38,12 @@ function showHelp() {
 
 	audioMotion -m "${ process.platform == 'win32' ? 'c:\\users\\john\\music' : '/home/john/music' }"
 
+	-b <path> : path to backgrounds directory (will replace built-in background images and videos)
+	-e        : allow external connections (by default, only localhost)
 	-m <path> : path to music directory
+	-nobg     : disable background images and videos
 	-p <port> : change server listening port (default: ${port})
 	-s        : start server only (do not launch client)
-	-e        : allow external connections (by default, only localhost)
 
 	` );
 }
@@ -71,53 +81,60 @@ function findImg( arr, pattern ) {
 
 /* main */
 
-console.log( `\n${serverSignature}` );
+console.log( `\n\t${serverSignature}\n\t${ ('=').repeat( serverSignature.length ) }` );
 
 if ( ! semver.gte( process.version, '10.10.0' ) ) {
-	console.log( `\n\nERROR: the minimum required version of node.js is v10.10.0 and you're running ${process.version}` );
+	console.log( `\n\n\t${ANSI_RED}%s${ANSI_RESET}`, `ERROR: the minimum required version of node.js is v10.10.0 and you're running ${process.version}` );
 	process.exit(0);
 }
 
 // processes command line arguments
-process.argv = process.argv.slice(2)
+
+process.argv = process.argv.slice(2);
+
 process.argv.forEach( ( arg, index ) => {
-	if ( arg == '-h' || arg == '--help' ) {
-		showHelp()
-		process.exit(0)
+	if ( arg == '-h' || arg.endsWith('-help') ) {
+		showHelp();
+		process.exit(0);
 	}
 	else if ( arg == '-m' && process.argv[ index + 1 ] )
-		musicPath = path.normalize( process.argv[ index + 1 ] )
+		musicPath = path.normalize( process.argv[ index + 1 ] );
+	else if ( arg == '-b' && process.argv[ index + 1 ] )
+		backgroundsPath = path.normalize( process.argv[ index + 1 ] );
 	else if ( arg == '-p' && process.argv[ index + 1 ] > 0 )
-		port = process.argv[ index + 1 ]
+		port = process.argv[ index + 1 ];
 	else if ( arg == '-s' )
-		launchClient = false
+		launchClient = false;
 	else if ( arg == '-e' )
-		host = ''
-})
+		host = '';
+	else if ( arg == '-nobg' )
+		backgroundsPath = false;
+});
 
 if ( ! musicPath ) {
-	console.log( '\n\n\tMusic folder not defined.\n\tUse the command-line argument -m <path> to set the folder upon launching audioMotion.' )
+	console.log( '\n\tMusic folder not defined.\n\tUse the command-line argument -m <path> to set the folder upon launching audioMotion.' );
 	musicPath = readlineSync.questionPath(
-		`\n\tPlease enter full path to music folder (e.g. ${ process.platform == 'win32' ? 'c:\\users\\john\\music' : '/home/john/music' })\n\tor just press Enter to use your home directory:\n\t> `, {
+		`\n\t${ANSI_GREEN}Please enter full path to music folder (e.g. ${ process.platform == 'win32' ? 'c:\\users\\john\\music' : '/home/john/music' })\n\tor just press Enter to use your home directory:\n\t> ${ANSI_RESET}`, {
 		isDirectory: true,
 		defaultInput: '~'
-	})
+	});
 }
 
 try {
-	fs.accessSync( musicPath, fs.constants.R_OK ) // check if music folder is readable
+	fs.accessSync( musicPath, fs.constants.R_OK ); // check if music folder is readable
 }
 catch (err) {
-	showHelp()
-	console.error( `\n\nERROR: no access to music folder at ${musicPath}` )
-	process.exit(0)
+	showHelp();
+	console.log( `\n\t${ANSI_RED}%s${ANSI_RESET}`, `ERROR: no access to music folder at ${musicPath}` );
+	process.exit(0);
 }
 
-// start Express web server
-const server = express(),
-	  pathname = path.join( __dirname, '../public' );
+// Express web server setup
 
-// returns directory index in a simple template compatible with our parseWebIndex() function
+const server     = express(),
+	  pathPublic = path.join( __dirname, '../public' );
+
+// helper function to return directory index in a simple template compatible with our parseWebIndex() function
 const indexTemplate = ( locals, callback ) => {
 	let htmlString = '<ul>';
 
@@ -129,8 +146,18 @@ const indexTemplate = ( locals, callback ) => {
 	callback( false, htmlString );
 }
 
-server.use( express.static( pathname ), serveIndex( pathname, { template: indexTemplate } ) );
+// set custom route for backgrounds folder
+if ( backgroundsPath != '' ) {
+	server.use( BG_DIR, express.static( backgroundsPath ), serveIndex( backgroundsPath, { template: indexTemplate } ) );
+	console.log( `\n\t${BG_DIR} folder mounted on ${ backgroundsPath }` );
+}
+// disable backgrounds folder (returns an empty listing)
+else if ( backgroundsPath === false ) {
+	server.use( BG_DIR, express.static( pathPublic ), serveIndex( pathPublic, { template: ( locals, callback ) => callback( false, '' ) } ) );
+	console.log( `\n\t${BG_DIR} folder disabled` );
+}
 
+// set route for /music folder
 server.use( '/music', express.static( musicPath ), ( req, res ) => {
 
 	let files = getDir( musicPath + decodeURI( req.url ).replace( /%23/g, '#' ) ),
@@ -147,21 +174,26 @@ server.use( '/music', express.static( musicPath ), ( req, res ) => {
 		files.cover = findImg( imgs, 'cover' ) || findImg( imgs, 'folder' ) || findImg( imgs, 'front' ) || imgs[0];
 		res.send( files );
 	}
-})
+});
 
+console.log( `\n\t/music folder mounted on ${musicPath}` );
+
+// set server root
+server.use( express.static( pathPublic ), serveIndex( pathPublic, { template: indexTemplate } ) );
+
+// start server
 server.listen( port, host, () => {
-	console.log( `\n\n\tListening on port ${port} ${ host ? 'for localhost connections only' : 'accepting external connections!' }` )
-	console.log( `\n\t/music mounted to ${musicPath}` )
+	console.log( `\n\n\t${ANSI_GREEN}%s${ANSI_RESET}`, `Listening on port ${port} ${ host ? 'for localhost connections only' : 'accepting external connections!' }` );
 	if ( launchClient ) {
-		open( `http://localhost:${port}` )
-		console.log( '\n\tLaunching client in browser...' )
+		open( `http://localhost:${port}` );
+		console.log( '\n\tLaunching client in browser...' );
 	}
-	console.log( '\n\nPress Ctrl+C to exit.' )
+	console.log( '\n\n\tPress Ctrl+C to terminate.' );
 })
 
 // route for custom server detection
 server.get( '/serverInfo', ( req, res ) => {
-	res.send( serverSignature )
+	res.send( serverSignature );
 })
 
 // route for retrieving a directory's cover picture
