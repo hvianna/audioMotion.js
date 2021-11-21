0705b81bc3d3cff9cb0914eb5a21e28b92ebb11f
Henrique Vianna <hvianna@gmail.com>
2021-11-21 15:58:27 -0300

    Add custom menu and About dialog; fix indentation.

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, shell } = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -10,33 +10,159 @@ const server = require('./server');
 
 console.log( server.port );
 
-const createWindow = () => {
-  const iconPath = path.resolve( __dirname, 'audioMotion.ico' );
-  const mainWindow = new BrowserWindow({
-    width: 1400,
-    height: 800,
-    resizable: true,
-    icon: iconPath,
-    webPreferences: {
-//      devTools: false,
-    }
-  });
+const isMac = process.platform === 'darwin';
 
-  mainWindow.loadURL( `http://localhost:${ server.port }/` );
+const template = [
+	// { role: 'appMenu' }
+	...(isMac ? [{
+		label: app.name,
+		submenu: [
+			{ role: 'about' },
+			{ type: 'separator' },
+			{ role: 'services' },
+			{ type: 'separator' },
+			{ role: 'hide' },
+			{ role: 'hideOthers' },
+			{ role: 'unhide' },
+			{ type: 'separator' },
+			{ role: 'quit' }
+		]
+	}] : []),
+	// { role: 'fileMenu' }
+	{
+		label: 'File',
+		submenu: [
+			isMac ? { role: 'close' } : { role: 'quit' }
+		]
+	},
+	// { role: 'editMenu' }
+/*
+	{
+		label: 'Edit',
+		submenu: [
+			{ role: 'undo' },
+			{ role: 'redo' },
+			{ type: 'separator' },
+			{ role: 'cut' },
+			{ role: 'copy' },
+			{ role: 'paste' },
+			...(isMac ? [
+				{ role: 'pasteAndMatchStyle' },
+				{ role: 'delete' },
+				{ role: 'selectAll' },
+				{ type: 'separator' },
+				{
+					label: 'Speech',
+					submenu: [
+						{ role: 'startSpeaking' },
+						{ role: 'stopSpeaking' }
+					]
+				}
+			] : [
+				{ role: 'delete' },
+				{ type: 'separator' },
+				{ role: 'selectAll' }
+			])
+		]
+	},
+*/
+	// { role: 'viewMenu' }
+	{
+		label: 'View',
+		submenu: [
+			{ role: 'reload' },
+			{ role: 'forceReload' },
+			{ role: 'toggleDevTools' },
+			{ type: 'separator' },
+			{ role: 'resetZoom' },
+			{ role: 'zoomIn' },
+			{ role: 'zoomOut' },
+			{ type: 'separator' },
+			{ role: 'togglefullscreen' }
+		]
+	},
+	// { role: 'windowMenu' }
+	{
+		label: 'Window',
+		submenu: [
+			{ role: 'minimize' },
+			{ role: 'zoom' },
+			...(isMac ? [
+				{ type: 'separator' },
+				{ role: 'front' },
+				{ type: 'separator' },
+				{ role: 'window' }
+			] : [
+				{ role: 'close' }
+			])
+		]
+	},
+	{
+		role: 'help',
+		submenu: [
+			{
+				label: "User's Manual",
+				click: async () => {
+					await shell.openExternal('https://audiomotion.me/users-manual');
+				}
+			},
+			{
+				label: 'Project on GitHub',
+				click: async () => {
+					await shell.openExternal('https://github.com/hvianna/audioMotion.js');
+				}
+			},
+			{
+				label: 'Website',
+				click: async () => {
+					await shell.openExternal('https://audiomotion.me');
+				}
+			},
+			{
+				label: 'About',
+				click: () => {
+					const { dialog } = require('electron');
+					dialog.showMessageBoxSync({
+						type: 'info',
+						title: 'About',
+						message: `audioMotion version ${ app.getVersion() }\nCopyright © 2018-2021 Henrique Avila Vianna`
+					});
+				}
+			}
+		]
+	}
+];
+
+const menu = Menu.buildFromTemplate( template );
+Menu.setApplicationMenu( menu );
+
+const createWindow = () => {
+	const iconPath = path.resolve( __dirname, 'audioMotion.ico' );
+	const mainWindow = new BrowserWindow({
+		width: 1400,
+		height: 800,
+		resizable: true,
+		icon: iconPath,
+		webPreferences: {
+//			devTools: false,
+		}
+	});
+
+	mainWindow.loadURL( `http://localhost:${ server.port }/` );
 };
 
 app.on( 'ready', createWindow );
 
 app.on( 'window-all-closed', () => {
-  if ( process.platform !== 'darwin' ) {
-    app.quit();
-  }
+	if ( process.platform !== 'darwin' ) {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if ( BrowserWindow.getAllWindows().length === 0 ) {
-    createWindow();
-  }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if ( BrowserWindow.getAllWindows().length === 0 ) {
+		createWindow();
+	}
 });
